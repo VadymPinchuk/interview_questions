2437a12bd48458b149b65b56716391001acd8ad8
Vadym Pinchuk <vad.pinchuk@gmail.com>
2020-05-09 12:31:57 +0300

    Update to insertion sort

--- lib/easy/insertion_sort.dart
+++ lib/easy/insertion_sort.dart
@@ -1,5 +1,7 @@
 void main() {
   insertionSort(<int>[2, 4, 7, 1, 6, 8, 2, 3, 5]);
+  insertionSort(<int>[2, 17, 7, 1, 9, -3, 2, 3, 15]);
+  insertionSort(<int>[2, 177, -17, 30, 9, -3, 2, 0, 155, 634, -12, 43, -12]);
 }
 
 void insertionSort(List<int> list) {
@@ -11,6 +13,8 @@ void insertionSort(List<int> list) {
         int tmp = list[i];
         list[i] = list[i + 1];
         list[i + 1] = tmp;
+      } else {
+        break;
       }
     }
     sorted++;
